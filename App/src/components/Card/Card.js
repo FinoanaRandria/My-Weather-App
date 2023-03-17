ee5543ea3e9a0d1542360c2ee1a5c3e623aab00e
FinoanaRandria <fitiavanarex4@gmail.com>
2023-03-17 22:59:40 +0300

    fix responsive

--- App/src/components/Card/Card.js
+++ App/src/components/Card/Card.js
@@ -9,7 +9,7 @@ import logo5 from "../Assets/Img/dew.png"
 function Card() {
     const [weather,setWeather]= useState({});
     const  [city,setCity]= useState({})
-    //const apikey = "//39ef9f6f8//20eb1c//31aad393df13b3f33"
+    const apikey = "39ef9f6f820eb1c31aad393df13b3f33"
      const getWeather = (e) =>{
       if (e.key === 'Enter'){
         fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`)
@@ -21,55 +21,71 @@ function Card() {
       }
      }
   return (
-    <div className='glass' >
+     
+    <>
+      <div className='container'>
+      <div className='glass' >
+      <div className='containt-head' >
+        <div>
+          
+          <h1 >Weather </h1>
+        </div>
+        <div>
+        <img  src={logo} alt="Salut ty" width={70} />
+        </div>
+      </div>
     
-      <h1 className='head'>Weather  <img className='logo' src={logo} alt="Salut ty" /></h1>
-    <div>
+  <div>
+       
+     <div className='containt-inp'>
+          <div className='input-containt'>
+            <input type="text"
+           className='search'
+           placeholder='Entrer ici le nom de la Ville'
+           value={city}
+            onChange = {(e) => setCity(e.target.value)}
+           onKeyPress ={getWeather}
+         />
+          </div>
          
-       <div>
-           <input type="text"
-             className='search'
-             placeholder='Entrer ici le nom de la Ville'
-             value={city}
-              onChange = {(e) => setCity(e.target.value)}
-             onKeyPress ={getWeather}
-           />
 
-           {
-            typeof weather.main != 'undefined'?
-            (
-              <div className='weather-container'>
-                <div className='location'>
-                <img src={logo2} alt="sun" width={30}/> {weather.name}, {weather.sys.country}
-                </div>
-                <div className='temp'>
-                <img src={logo4} alt="sun" width={30}/> {Math.round(weather.main.temp)}°C
-                </div>
-                <div className='weather'>
-                <img src={logo1} alt="sun" width={30}/>  {weather.weather[0].main}
-                </div>
-                <div className='wind'>
-                <img src={logo3} alt="sun" width={30}/>  {weather.wind.speed} m/s
-                </div>
-                <div className='humid' >
-                   <img src={logo5} alt="humid" width={30}/> {weather.main.humidity} g/m3
-                </div>
-
-                
+         {
+          typeof weather.main != 'undefined'?
+          (
+            <div className='weather-container'>
+              <div className='location'>
+              <img src={logo2} alt="sun" width={30}/> {weather.name}, {weather.sys.country}
               </div>
-            ) 
-            :
-            (
-
-              <div className='weather-container'>
-                <h1>Bien Sur Weather-App</h1>
+              <div className='temp'>
+              <img src={logo4} alt="sun" width={30}/> {Math.round(weather.main.temp)}°C
               </div>
-            )
-           }
-       </div>
-    </div>
-          <p>@Create and designed by Finoana_Randria </p>
-    </div>
+              <div className='weather'>
+              <img src={logo1} alt="sun" width={30}/>  {weather.weather[0].main}
+              </div>
+              <div className='wind'>
+              <img src={logo3} alt="sun" width={30}/>  {weather.wind.speed} m/s
+              </div>
+              <div className='humid' >
+                 <img src={logo5} alt="humid" width={30}/> {weather.main.humidity} g/m3
+              </div>
+
+              
+            </div>
+          ) 
+          :
+          (
+
+            <div className='weather-container'>
+              <h1>Bienvenu Sur Weather-App</h1>
+            </div>
+          )
+         }
+     </div>
+  </div>
+        <p>@Create and designed by Finoana_Randria </p>
+  </div>
+      </div>
+    </>
     
   )
 }
